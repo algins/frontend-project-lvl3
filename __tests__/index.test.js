75a629c65470739da90796e04f3d5769ef93c670
algins <igors.algins@gmail.com>
2022-05-13 12:45:37 +0300

    Fix no-underscore-dangle

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,9 +3,9 @@ import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import init from '../src/init.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getFixturePath = (filename) => join(__dirname, '..', '__tests__', '__fixtures__', filename);
+const fileUrl = fileURLToPath(import.meta.url);
+const dirName = dirname(fileUrl);
+const getFixturePath = (fileName) => join(dirName, '..', '__tests__', '__fixtures__', fileName);
 
 beforeEach(async () => {
   const pathToHtml = getFixturePath('index.html');
